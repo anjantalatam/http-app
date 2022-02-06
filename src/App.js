3a0fd0f99e247a89a1a0f5483b1dedc8ba3773c2
Anjan Talatam <tspa2117@gmail.com>
2022-02-06 17:35:25 +0530

    8.9 Optimistic vs Pessimistic Approach

--- src/App.js
+++ src/App.js
@@ -34,10 +34,17 @@ class App extends Component {
   };
 
   handleDelete = async (post) => {
-    await axios.delete(`${apiEndpoint}/${post.id}`);
-
+    const originalState = this.state.posts;
     const posts = this.state.posts.filter((p) => p.id !== post.id);
     this.setState({ posts });
+
+    try {
+      await axios.delete(`${apiEndpoint}/${post.id}`);
+      throw new Error("");
+    } catch (error) {
+      alert("Operation Failed!");
+      this.setState({ posts: originalState });
+    }
   };
 
   render() {
